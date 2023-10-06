58398beedcfc9855d9ca1c7744005c004b495da8
sanskarsingh_iitbhu21 <97400210+SanskarSinghiit@users.noreply.github.com>
2023-10-06 19:12:00 +0530

    Time: 0 ms (100%), Space: 6.4 MB (36.38%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+
+    int func(int n, int k, vector<int>& vec)
+    {
+        int x = n/k;
+        for(int i = 0; i < k; i++)
+        {
+            vec.push_back(x);
+        }
+        int y = n % k;
+        // cout << " y: " << y << '\n';
+        // return 0; 
+        for(int i = 0; i < y; i++)
+        {
+            vec[i]++;
+        }
+        // return 0;
+        int product = 1;
+        for(int i = 0; i < k; i++)
+        {
+            product *= vec[i];
+        }
+        vec.clear();
+        return product;
+    }
+
+    int integerBreak(int n)
+    {
+        // n = 10;
+        // int k = 4;
+        vector<int> vec;
+        // cout << func(n, k, vec);
+        int maxi = INT_MIN;
+        for(int i = 2; i <= n; i++)
+        {
+            int z = func(n, i, vec);
+            maxi = maxi < z ? z : maxi;
+        }
+        return maxi;
+        // return 0;
+    }
+};
