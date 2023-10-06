24881dcfd81439278bf3ee2abaf5de87c07f4d82
sanskarsingh_iitbhu21 <97400210+SanskarSinghiit@users.noreply.github.com>
2023-10-06 19:21:43 +0530

    Time: 2 ms (49.96%), Space: 6.4 MB (36.38%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,44 +1,33 @@
 class Solution {
 public:
 
-    int func(int n, int k, vector<int>& vec)
+    int func(int n, int k)
     {
-        int x = n/k;
-        for(int i = 0; i < k; i++)
-        {
-            // vec.push_back(x);
-            vec[i] = x;
-        }
-        int y = n % k;
-        // cout << " y: " << y << '\n';
-        // return 0; 
-        for(int i = 0; i < y; i++)
-        {
-            vec[i]++;
-        }
-        // return 0;
+        int x = n/k; // the base numbers;
+        int y = n%k; // the extras;
         int product = 1;
         for(int i = 0; i < k; i++)
         {
-            product *= vec[i];
+            if(y>0)
+            {
+                product *= (x+1); y--;
+            }
+            else
+            {
+                product *= x;
+            }
         }
-        // vec.clear();
         return product;
     }
 
     int integerBreak(int n)
     {
-        // n = 10;
-        // int k = 4;
-        vector<int> vec(n, 1);
-        // cout << func(n, k, vec);
         int maxi = INT_MIN;
         for(int i = 2; i <= n; i++)
         {
-            int z = func(n, i, vec);
+            int z = func(n, i);
             maxi = maxi < z ? z : maxi;
         }
         return maxi;
-        // return 0;
     }
 };
