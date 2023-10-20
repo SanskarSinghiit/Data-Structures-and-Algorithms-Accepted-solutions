39c1e39c1ddfd9c822a95e010ef8698b528184d7
sanskarsingh_iitbhu21 <97400210+SanskarSinghiit@users.noreply.github.com>
2023-10-21 00:04:51 +0530

    Time: 72 ms (10.1%), Space: 6.5 MB (39.38%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    bool check10(int n)
+    {
+        int x = (int)(log10(n));
+        while(x--)
+        {
+            int x = n%10;
+            if(x!=0){return false;}
+            n /= 10;
+        }
+        if(n==1){return true;}
+        return false;
+    }
+
+    int makeIt(int n)
+    {
+        int x = (int)(log10(n))+1;
+        int sum = 0;
+        while(x--)
+        {
+            int p = n%10;
+            sum += p*p;
+            n/=10;
+        }
+        return sum;
+    }
+
+    bool isHappy(int n)
+    {
+        int len = (int)(log10(n))+1;
+        int stop = 0;
+        while(check10(n)==false)
+        {
+            n = makeIt(n);
+            stop++;
+            if(stop>10000){return false;}
+        }
+        return true;
+    }
+};
