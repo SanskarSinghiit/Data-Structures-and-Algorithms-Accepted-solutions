339fb23be55e41e3b38a8a35088adc0eebdd33f0
sanskarsingh_iitbhu21 <97400210+SanskarSinghiit@users.noreply.github.com>
2023-12-03 00:29:56 +0530

    Time: 44 ms (78.41%) | Memory: 20.8 MB (64.93%) - LeetSync

---
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int countCharacters(vector<string>& words, string chars)
+    {
+        vector<int> hmp(26,0);
+        int l = chars.size();
+        for(int i = 0; i < l; i++)
+        {
+            hmp[chars[i]-'a']++;
+        }
+
+        int n = words.size();
+        int cnt = 0;
+        for(int i = 0; i < n; i++)
+        {
+            // if not present then continue;
+            vector<int> dummy = hmp;
+            bool cont = false;
+            for(int j = 0; j < words[i].size(); j++)
+            {
+                char c = words[i][j];
+                if(dummy[c-'a'] <= 0){cont=true; break;}
+                else{dummy[c-'a']--;}
+            }
+            if(cont){continue;}
+            // if present and then cnt++
+            cnt += words[i].size();
+        }
+        return cnt;
+    }
+};
