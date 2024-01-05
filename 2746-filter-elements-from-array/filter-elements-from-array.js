ee7b3fe47347086d45f6b75b668a966fe54c5938
sanskarsingh_iitbhu21 <97400210+SanskarSinghiit@users.noreply.github.com>
2024-01-05 17:59:51 +0530

    Time: 50 ms (69.11%) | Memory: 41.8 MB (55.13%) - LeetSync

--- 2746-filter-elements-from-array/filter-elements-from-array.js
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -4,6 +4,12 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    arr = arr.filter(fn);
-    return arr;
+    // arr = arr.filter(fn);
+    let arr1 = [];
+    for(var i = 0; i < arr.length; i++){
+        if(fn(arr[i], i)){
+            arr1.push(arr[i]);
+        }
+    }
+    return arr1;
 };
