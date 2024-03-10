26e71d16943e7a4ee14fcd1e6245b1659e9c8b1a
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-03-10 22:27:18 +0530

    Time: 19 ms (44.46%) | Memory: 15.7 MB (11.69%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string str)
+    {
+        int n = str.size();
+        int j = 0;
+        set<char> st;
+        int maxi = 0;
+        for(int i = 0; i < n; i++){
+            if(j==n){
+                break;
+            }
+            char c = str[j];
+            // cout << "i->" << i << " st->";
+            // for(auto val : st){
+            //     cout << val << ' ';
+            // } cout << '\n';
+            while(st.find(str[j])==st.end()){
+                st.insert(str[j]);
+                maxi = j-i+1 > maxi ? j-i+1 : maxi;
+                // cout << "maxi->" << maxi << '\n';
+                j++;
+                if(j==n){
+                    break;
+                }
+                // c = str[j];
+            }
+            if(st.find(str[j])!=st.end()) {
+                st.erase(st.find(str[i]));
+            }
+            // cout << "i->" << i << " st->";
+            // for(auto val : st){
+            //     cout << val << ' ';
+            // } cout << '\n' << '\n';
+        }
+        return maxi;
+    // longest substring so no child concept; OK
+    //     set<char> s;
+    //     int n = str.size();
+    //     int longest = 0;
+    //     int i = 0; int j = 0;
+    //     while(j<n)  // ratt lo ki j < n ;
+    //     {
+    //         char c = str[j];
+    //         cout << "i-> "<<i<< " c->" << c <<'\n'; 
+    //         if(s.find(c)==s.end())
+    //         {
+    //             s.insert(c);
+    //             longest = (j-i+1 > longest) ? (j-i+1) : (longest);
+    //             j++;
+    //         }
+    //         else
+    //         {
+    //             while(s.find(c) != s.end())
+    //             {
+    //                 cout << "else : c->" << c << " i->" << i << ' ';
+    //                 cout << "hello ";
+    //                 auto it = s.begin();
+    //                 cout << "it->" << *it << '\n';
+    //                 s.erase(it);
+    //                 i++;
+    //             }
+    //             s.insert(c);
+    //             longest = (j-i+1 > longest) ? (j-i+1) : (longest);
+    //             j++;
+    //         }
+    //     }
+    //     return longest;
+    }
+};
