c93dd9b1bb844b6a561eddcafc1c808967dc0272
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-05-22 20:04:35 +0530

    Time: 307 ms (5.00%) | Memory: 251.8 MB (5.04%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -0,0 +1,78 @@
+class Solution {
+public:
+
+    bool check(vector<string>& vec){
+        int n = vec.size();
+        for(auto val : vec){
+            if(!isPalindrome(val)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool isPalindrome(string& str){
+        int n = str.size();
+        string str1 = "";
+        for(int i = n-1; i >= 0; i--){
+            str1.push_back(str[i]);
+        }
+        return str==str1;
+    }
+
+    void rec(string& s, vector<string> op, int i, vector<vector<string>>& allPartitions, int& count){
+        // cout << " i -> " << i << "  ";
+        //             for(auto val : op){
+        //         cout << val << ' ';
+        //     } cout << '\n';
+        if(i==0){
+            string temp = "";
+            temp.push_back(s[0]);
+            // op.push_back(string() + s[0]);
+            op.push_back(temp);
+            rec(s, op, i+1, allPartitions, count);
+            return;
+        }
+        if(i==s.size()){
+            // allPartitions.insert(op);
+            allPartitions.push_back(op);
+            // cout << " i->" << i << ' ' << count++ << " => ";
+            // for(auto val : op){
+            //     cout << val << ' ';
+            // } cout << '\n';
+            op.clear();
+            return;
+        }
+        op.back().push_back(s[i]);
+        rec(s, op, i+1, allPartitions, count);
+        op.back().pop_back();
+        string temp = "";
+        temp.push_back(s[i]);
+        // op.push_back(string() + s[i]);
+        op.push_back(temp);
+        rec(s, op, i+1, allPartitions, count);
+    }
+
+    vector<vector<string>> partition(string s) {
+        // set<vector<string>> allPartitions;
+        vector<vector<string>> allPartitions;
+        vector<vector<string>> ans;
+        vector<string> op;
+        int count = 0;
+        rec(s, op, 0, allPartitions, count);
+        for(auto val : allPartitions){
+            if(check(val)){
+                ans.push_back(val);
+            }
+        }
+        return ans;
+    }
+};
+
+/*
+
+=> All individual letters are palindromes
+=> Call a recursive function to generate all possible partitioning of the given string
+=> Store all partitionings somewhere and extract all partitioning which are palindrome 
+
+*/
