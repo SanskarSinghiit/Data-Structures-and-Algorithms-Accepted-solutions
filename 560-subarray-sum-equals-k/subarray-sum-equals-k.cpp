c252eb17f5792c99da639f054e0bcbf8deef94c9
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-06-03 00:16:56 +0530

    Time: 2804 ms (5.01%) | Memory: 32.9 MB (87.69%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,12 +1,5 @@
 class Solution {
 public:
-
-    void print(int arr[], int n){
-        for(int i = 0; i < n; i++){
-            cout << arr[i] << ' ';
-        } cout << '\n';
-    }
-
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
         int prefix[n];
@@ -14,25 +7,57 @@ public:
         for(int i = 1; i < n; i++){
             prefix[i] = prefix[i-1] + nums[i];
         }
-        // cout << "printing prefix : " << '\n';
-        // print(prefix, n);
         int count = 0;
         for(int i = 0; i < n; i++){
             for(int j = i; j < n; j++){
-                // cout << " i => " << i << ' ' << " j => " << j << "  ";
-                 int sum = 0;
+                int sum = 0;
                 if(i==0){
                     sum = prefix[j];
                 } else{
                     sum = prefix[j] - prefix[i-1];
                 }
-
-                if(sum == k){
+                if(sum==k){
                     count++;
                 }
-                // cout << "sum => " << sum << '\n';
             }
         }
         return count;
     }
 };
+
+
+// class Solution {
+// public:
+
+//     void print(int arr[], int n){
+//         for(int i = 0; i < n; i++){
+//             cout << arr[i] << ' ';
+//         } cout << '\n';
+//     }
+// // THIS CODE DOES IT IN O(N^2), USING PREFIX SUM
+//     int subarraySum(vector<int>& nums, int k) {
+//         int n = nums.size();
+//         int prefix[n];
+//         prefix[0] = nums[0];
+//         for(int i = 1; i < n; i++){
+//             prefix[i] = prefix[i-1] + nums[i];
+//         }
+//         int count = 0;
+//         for(int i = 0; i < n; i++){
+//             for(int j = i; j < n; j++){
+//                  int sum = 0;
+//                 if(i==0){
+//                     sum = prefix[j];
+//                 } else{
+//                     sum = prefix[j] - prefix[i-1];
+//                 }
+
+//                 if(sum == k){
+//                     count++;
+//                 }
+//                 // cout << "sum => " << sum << '\n';
+//             }
+//         }
+//         return count;
+//     }
+// };
