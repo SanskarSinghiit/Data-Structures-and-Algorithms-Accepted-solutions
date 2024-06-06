6107d832a04f19b97e344f96b26b09c1e4afc3a5
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-06-07 04:40:46 +0530

    Time: 76 ms (17.12%) | Memory: 31.5 MB (33.43%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        int n = hand.size();
+        int x = groupSize;
+        sort(hand.begin(), hand.end());
+        map<int, int> mp;
+        for(int i = 0; i < n; i++){
+            mp[hand[i]]++;
+        }
+        while(mp.empty() == false){
+            int val = (mp.begin())->first;
+            cout <<  val << ' ';
+            mp[val]--;
+            if(mp[val] == 0){
+                // cout << "got : " << val << " as zero" << " ";
+                mp.erase(mp.find(val));
+            }
+            // cout << mp.find(val) << " ";
+            // if(mp.find(val) == mp.end()){
+            //     cout << "deleted " << val << ' ';
+            // }
+            // cout << "printing map => " << '\n';
+            // for(auto val : mp){
+            //     cout << val.first << ' ' << val.second << '\n';
+            // }
+            // cout << '\n';
+            for(int i = 1; i < x; i++){
+                int finds = val + i;
+                // cout << finds << ' ';
+                if(mp[finds] == 0){
+                    return false;
+                } else{
+                    mp[finds]--;
+                    if(mp[finds] == 0){
+                        mp.erase(mp.find(finds));
+                    }
+                }
+            }
+            cout << '\n';
+        }
+        return true;
+    }
+};
