ff971fa27af55bd6c7af24b9191e1abb4c5119ab
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-06-07 04:42:05 +0530

    Time: 155 ms (33.74%) | Memory: 54.8 MB (28.50%) - LeetSync

--- 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.cpp
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -13,21 +13,10 @@ public:
             cout <<  val << ' ';
             mp[val]--;
             if(mp[val] == 0){
-                // cout << "got : " << val << " as zero" << " ";
                 mp.erase(mp.find(val));
             }
-            // cout << mp.find(val) << " ";
-            // if(mp.find(val) == mp.end()){
-            //     cout << "deleted " << val << ' ';
-            // }
-            // cout << "printing map => " << '\n';
-            // for(auto val : mp){
-            //     cout << val.first << ' ' << val.second << '\n';
-            // }
-            // cout << '\n';
             for(int i = 1; i < x; i++){
                 int finds = val + i;
-                // cout << finds << ' ';
                 if(mp[finds] == 0){
                     return false;
                 } else{
@@ -37,7 +26,6 @@ public:
                     }
                 }
             }
-            cout << '\n';
         }
         return true;    
     }
