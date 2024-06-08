31725ddf9f631b24c822b0c03277f1c12ddd6c53
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-06-09 03:50:00 +0530

    Time: 91 ms (43.32%) | Memory: 47.6 MB (35.61%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -2,6 +2,36 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
+        vector<int> prefix(n, -1);
+        int count = 0;
+        map<int, int> mp;
+        prefix[0] = nums[0];
+        mp[prefix[0]]++;
+        if(prefix[0] == k){
+            count++;
+        }
+        for(int i = 1; i < n; i++){
+            prefix[i] = prefix[i-1] + nums[i];
+            if(prefix[i] == k){
+                count++;
+            } 
+            if(mp.find(prefix[i] - k) != mp.end()){
+                count+=mp[prefix[i]-k];
+            } 
+            mp[prefix[i]]++;
+        }
+        return count;
+    }
+};
+
+
+/*  THIS CODE IS OF O(N^2), 
+    HWO TO DO IT IN O(NLOGN)
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
         int prefix[n];
         prefix[0] = nums[0];
         for(int i = 1; i < n; i++){
@@ -23,7 +53,7 @@ public:
         }
         return count;
     }
-};
+};  */
 
 
 // class Solution {
