f446dc1fbf63e2b235c1c4310d724fc484d49910
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-06-16 23:02:52 +0530

    Time: 441 ms (41.96%) | Memory: 11.3 MB (91.66%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        // bool isPrime[n+1] = {true};
+        vector<bool> isPrime(n+1, true);
+        isPrime[0] = isPrime[1] = false;
+        for(int i = 2; i <= sqrt(n); i++){
+            if(isPrime[i]){
+                for(int j = i*i; j <= n; j+=i){
+                    isPrime[j] = false;
+                }
+            }
+        }
+        int count = 0;
+        for(int i = 0; i < n; i++){
+            if(isPrime[i]){count++;}
+        }
+        // cout << "for 2 => " << isPrime[2] << '\n';
+        return count;
+    }
+};
