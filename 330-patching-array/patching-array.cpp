c7f30f91d330433a701efb29bf4f89b7a1a88abe
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-06-17 03:40:36 +0530

    Time: 6 ms (36.35%) | Memory: 13.7 MB (82.73%) - LeetSync

---
+++ 330-patching-array/patching-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+int minPatches(vector<int> &nums, int n){
+    long long coverage = 0;
+    int m = nums.size();
+    long long patch = 0;
+    long long j = 0;
+    long long i = 1;
+    while(i<=n){
+        // if(i <= coverage){continue;}
+        if ((j < m) && (nums[j] <= i))
+        {
+            coverage += nums[j];
+            j++;
+        }
+        else
+        {
+            // patch(i);
+            patch++;
+            coverage += i;
+        }
+        i = coverage + 1;
+    }
+    return patch;
+}
+}; 
