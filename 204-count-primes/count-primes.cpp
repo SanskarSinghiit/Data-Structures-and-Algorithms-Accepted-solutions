320f480b38f2289f2f790cac78ce54072b3f5292
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-06-17 17:52:17 +0530

    Time: 515 ms (30.17%) | Memory: 11.6 MB (44.23%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -2,20 +2,35 @@ class Solution {
 public:
     int countPrimes(int n) {
         // bool isPrime[n+1] = {true};
-        vector<bool> isPrime(n+1, true);
-        isPrime[0] = isPrime[1] = false;
-        for(int i = 2; i <= sqrt(n); i++){
-            if(isPrime[i]){
-                for(int j = i*i; j <= n; j+=i){
-                    isPrime[j] = false;
-                }
+    //     vector<bool> isPrime(n+1, true);
+    //     isPrime[0] = isPrime[1] = false;
+    //     for(int i = 2; i <= sqrt(n); i++){
+    //         if(isPrime[i]){
+    //             for(int j = i*i; j <= n; j+=i){
+    //                 isPrime[j] = false;
+    //             }
+    //         }
+    //     }
+    //     int count = 0;
+    //     for(int i = 0; i < n; i++){
+    //         if(isPrime[i]){count++;}
+    //     }
+    //     return count;
+    // }
+    // bool isPrime[n+1] = {true};
+    vector<bool> isPrime(n+1, true);
+    isPrime[0] = isPrime[1] = false;
+    for(int i = 2; i <= sqrt(n); i++){
+        if(isPrime[i]){
+            for(int j = i*i; j <= n; j+=i){
+                isPrime[j] = false;
             }
         }
-        int count = 0;
-        for(int i = 0; i < n; i++){
-            if(isPrime[i]){count++;}
-        }
-        // cout << "for 2 => " << isPrime[2] << '\n';
-        return count;
+    }
+    int count = 0;
+    for(int i = 0; i < n; i++){
+        if(isPrime[i]){count++;}
+    }
+    return count;
     }
 };
