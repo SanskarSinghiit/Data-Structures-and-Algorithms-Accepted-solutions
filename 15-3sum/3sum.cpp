9f861cf1d701998f68a8dfa13c1f4d002f49f39b
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-07-09 22:27:46 +0530

    Time: 72 ms (42.97%) | Memory: 36.1 MB (28.49%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -13,6 +13,12 @@ public:
             } else if(sum < target){
                 i++;
             } else{
+                while(i < n-1 && nums[i] == nums[i+1]){
+                    i++;
+                }
+                while(j>1 && nums[j] == nums[j-1] ){
+                    j--;
+                }
                 temp.push_back(-1*target);
                 temp.push_back(nums[i]);
                 temp.push_back(nums[j]);
@@ -26,22 +32,26 @@ public:
     }
 
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
         int n = nums.size();
         set<vector<int>> st;
         for(int i = 0; i < n; i++){
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
+            }
             int x = nums[i];
             vector<vector<int>> temp = twoSum(-1*x, i+1, n, nums);
             if(temp.empty()){
                 continue;
             }
             // sort(temp.begin(), temp.end());
+            // ans.push_back(temp);
             for(auto val : temp){
-                st.insert(val);
+                ans.push_back(val);
             }
             // st.insert(temp);
         }
-        vector<vector<int>> ans;
         // set<vector<int>> us;
         // int n = nums.size();
         // for(int i = 0; i < n; i++){
@@ -56,9 +66,9 @@ public:
         //         }
         //     }
         // }
-        for(auto val : st){
-            ans.push_back(val);
-        }
+        // for(auto val : st){
+        //     ans.push_back(val);
+        // }
         return ans;
     }
 };
