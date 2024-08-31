295fe291eb174d185122a277c1009d07f2a58cdf
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-08-31 11:53:22 +0530

    Time: 8 ms (47.16%) | Memory: 16.8 MB (79.71%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,39 +1,33 @@
+#define vvi vector<vector<int>>
+
 class Solution {
 public:
 
-    void bfs(vector<vector<int>>& image, int root, int color, int sr, int sc){
-        queue<pair<int, int>> q;
-        int n = image.size();
-        int m = image[0].size();
-        q.push({sr, sc});
-        image[sr][sc] = color;
-        int a[4] = {-1, 0, 1, 0};
-        int b[4] = {0, 1, 0, -1};
-        while(!q.empty()){
-            auto node = q.front();
-            q.pop();
-            int x = node.first;
-            int y = node.second;
-            for(int i = 0; i < 4; i++){
-                int x1 = x + a[i];
-                int y1 = y + b[i];
-                if(x1>=n || y1>=m || x1<0 || y1 <0){
-                    continue;
-                } else if(image[x1][y1] != root){
-                    continue;
-                }
-                image[x1][y1] = color;
-                q.push({x1, y1});
-            }
+    // vis if a pixel has color == color, 
+
+    void rec(int x, int y, int color, int scolor, vvi& image, vvi& ans, int n, int m){
+        if(x<0 || x>=n || y<0 || y>=m || image[x][y]!=scolor || ans[x][y]!=-1){return;}
+        ans[x][y]=color;
+        int dx[4]={0, 1, 0, -1};
+        int dy[4]={-1, 0, 1, 0};
+        for(int i = 0; i < 4; i++){
+            int cx = x + dx[i];
+            int cy = y + dy[i];
+            rec(cx, cy, color, scolor, image, ans, n, m);
         }
     }
 
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int root = image[sr][sc];
-        if(root == color){
-            return image;
+        int scolor = image[sr][sc];
+        int n = image.size();
+        int m = image[0].size();
+        vector<vector<int>> ans(n, vector<int>(m, -1)); // -1 is unvisited
+        rec(sr, sc, color, scolor, image, ans, n, m);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(ans[i][j]==-1){ans[i][j]=image[i][j];}
+            }
         }
-        bfs(image, root, color, sr, sc);
-        return image;
+        return ans;
     }
 };
