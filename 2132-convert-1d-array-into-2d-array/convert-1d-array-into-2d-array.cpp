c1f17c9545e22984abe2286bf90244fc854259c6
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-09-01 11:00:02 +0530

    Time: 107 ms (5.58%) | Memory: 92.8 MB (25.95%) - LeetSync

---
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        int sz = original.size();
+        vector<vector<int>> _2d(m, vector<int>(n));
+        if(sz != m*n){return {};}
+        // cout << "original -> " << sz << '\n';
+        for(int i = 0; i < sz; i++){
+            // cout << i/n << ' ' << i%n  << ' ' << i << '\n';
+            _2d[i/n][i%n]=original[i];
+            // cout << _2d[i/n][i%n] << '\n';
+        } return _2d;
+    }
+};
