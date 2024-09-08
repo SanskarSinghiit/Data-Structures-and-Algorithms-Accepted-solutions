60c66475760821188d2c1ef233189e822d7ec73e
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-09-08 15:20:34 +0530

    Time: 21 ms (14.96%) | Memory: 20.3 MB (58.10%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
+    // so the best approach is do a left pass, and finalise all increasing seq;
+
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        int count = 0;
-        vector<int> candy(n, 1);
-        for(int i = 1; i < n; i++){
-            if(ratings[i] > ratings[i-1]){
+        vector<int> candy(n, 1); // 0 means unassigned
+        for(int i=1; i <n; i++){
+            if(ratings[i]>ratings[i-1]){
                 candy[i]=candy[i-1]+1;
             }
-        }
-        for(int i = n-2; i>=0; i--){
-            if(ratings[i] > ratings[i+1]){
+        }for(int i = n-2; i >= 0; i--){
+            if(ratings[i]>ratings[i+1]){
                 candy[i]=max(candy[i],candy[i+1]+1);
             }
         }
-        for(int i = 0; i < n; i++){
-            count += candy[i];
-        } return count;
+        int candies=0;
+        for(auto val : candy){candies+=val;}
+        return candies;
+        
     }
 };
