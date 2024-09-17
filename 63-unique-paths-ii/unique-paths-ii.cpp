81b25305e24ddaff6113ebb0e8d95141e3184d94
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-09-17 20:55:31 +0530

    Time: 6 ms (20.95%) | Memory: 10.6 MB (41.34%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+
+    int mod = 2*1e9;
+
+    int rec(int x, int y, vector<vector<int>>& dp, int m, int n){
+        if(dp[x][y] != -1){return dp[x][y];}
+        int a = 0;
+        int b = 0;
+        if(x+1<m){
+            a = rec(x+1, y, dp, m, n);
+        } 
+        if(y+1<n){
+            b = rec(x, y+1, dp, m, n);
+        }
+        dp[x][y]=(a%mod + b%mod)%mod;
+        // cout << "x->"<< x << ' ' << "y->"<<y<<" dp[x][y]-> " << dp[x][y] << '\n';
+        // if(x==2&&y==0&&m==4&&n==4){
+        //     cout << "this is 2, 0, and this is dp[3][0] -> " << dp[3][0] << " and dp[2][1] -> " << dp[2][1] << '\n';
+        //     cout << "this is a-> " << a << " and this is b -> " << b << '\n';
+        //     cout << "this is a_m+b_m_m -> " << (a%m + b%m)%m << '\n';
+        // }
+        return dp[x][y];
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        if(obstacleGrid[m-1][n-1] || obstacleGrid[0][0]){return 0;}
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(obstacleGrid[i][j]){
+                    dp[i][j]=0;
+                }
+            }
+        }
+
+        dp[m-1][n-1]=1;
+        return dp[0][0] = rec(0, 0, dp, m, n);
+    }
+};
