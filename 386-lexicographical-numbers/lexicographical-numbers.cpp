cacd9425ff70df15f08942812a3fa92856fbd2f8
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-09-21 16:18:23 +0530

    Time: 3 ms (94.22%) | Memory: 13.8 MB (55.23%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,15 +1,39 @@
 class Solution {
 public:
+
+    void rec(int num, int sz, vector<int>& ans, int n){
+        if(sz==0){ans.push_back(num); return;}
+        if(num>n){return;}
+        ans.push_back(num);
+        num*=10;
+        for(int i = 0; i <= 9; i++){
+            // int temp = num*10;
+            // temp+=i;
+            num+=i;
+            if(num>n){return;}
+            rec(num, sz-1, ans, n);
+            num-=i;
+        }
+    }
+
     vector<int> lexicalOrder(int n) {
         // bruteforce with extra space
-        vector<string> v;
-        for(int i = 1; i <= n; i++){
-            v.push_back(to_string(i));
-        }
-        sort(v.begin(), v.end());
+        // vector<string> v;
+        // for(int i = 1; i <= n; i++){
+        //     v.push_back(to_string(i));
+        // }
+        // sort(v.begin(), v.end());
+        // vector<int> ans;
+        // for(auto str : v){
+        //     ans.push_back(stoi(str));
+        // }
+
+        // optimised with no extra space
+        int sz = (int)(log10(n)+1);
         vector<int> ans;
-        for(auto str : v){
-            ans.push_back(stoi(str));
+        for(int i = 1; i <= 9; i++){
+            if(i>n){break;}
+            rec(i, sz-1, ans, n);
         }
         return ans;
     }
