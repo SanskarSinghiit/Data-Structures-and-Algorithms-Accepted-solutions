a36fe749ee5c7890982024d75d2181ed19a3331f
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-09-27 22:55:39 +0530

    Time: 110 ms (46.22%) | Memory: 13.2 MB (70.79%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
-    int lengthOfLIS(std::vector<int>& nums) {
-        if (nums.empty()) {
-            return 0;
-        }
 
-        int n = nums.size();
-        std::vector<int> dp(n, 1);
-
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] > nums[j]) {
-                    dp[i] = std::max(dp[i], dp[j] + 1);
-                }
+    int f(int i, int dp[], vector<int>& nums){
+        if(i==0){return 1;}
+        if(dp[i]!=-1){return dp[i];}
+        int maxi = 0;
+        for(int j = i-1; j >= 0; j--){
+            if(nums[j] < nums[i]){
+                int x = f(j, dp, nums);
+                if(x>maxi){maxi=x;}
             }
         }
+        return dp[i]=1+maxi;
+    }
 
-        return *std::max_element(dp.begin(), dp.end());
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        int dp[n+1];
+        memset(dp, -1, sizeof(dp));
+        int maxi = INT_MIN;
+        for(int i = n-1; i >= 0; i--){
+            int x = f(i, dp, nums);
+            if(x>maxi){maxi=x;}
+        }
+        // int ans = f(n-1, dp, nums);
+        // cout << "this is the dp array -> " << '\n';
+        // for(auto val : dp){
+        //     cout << val << ' ';
+        // } cout << '\n';
+        return maxi;
+        // return f(n-1, dp, nums);
     }
 };
