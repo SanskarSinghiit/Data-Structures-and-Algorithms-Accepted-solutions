982948e91931ddaf799aa0a58c3580d940e52912
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-09-28 12:41:40 +0530

    Time: 410 ms (5.02%) | Memory: 57.1 MB (7.82%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+
+    int f(int amount, int dp[], vector<int>& coins){
+        if(amount==0){return dp[amount]=0;}
+        if(amount<0){return INT_MAX;}
+        if(dp[amount]!=-1){return dp[amount];}
+        
+        int mini = INT_MAX;
+        for(auto val : coins){
+            int x = f(amount-val, dp, coins);
+            // if(x==-1){continue;}
+            if(x<mini){mini=x;}
+        }
+        if(mini==INT_MAX){
+            return dp[amount]=INT_MAX;
+        }
+        return dp[amount]=1+mini;
+    }
+
+    int coinChange(vector<int>& coins, int amount) {
+        int dp[10000000];
+        memset(dp, -1, sizeof(dp));
+        int n = coins.size();
+        // for(auto val : coins){dp[val]=1;}
+        int ans = f(amount, dp, coins);
+        return ans==INT_MAX ? -1 : ans;
+        // return f(amount, dp, coins) == INT_MAX ? -1 :;
+    }
+};
