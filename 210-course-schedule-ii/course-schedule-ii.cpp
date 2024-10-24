e2565b94e59151a6e73246c535f98fc4580968c9
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-10-24 16:15:58 +0530

    Time: 3 ms (98.10%) | Memory: 19.7 MB (8.73%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,45 +1,42 @@
+#define v vector
 class Solution {
 public:
 
-    void dfs(int node, vector<int> adj[], stack<int>& stk, bool vis[], bool pathVis[], bool& cycle){
-        if(cycle){return;}
-        if(vis[node] && pathVis[node]){
-            cycle = true;
-            return;
-        } else if(vis[node]){
-            return;
-        } // it is never possible that a node is pathVis but not vis, as pathVis, vis = true simultaneously and once vis, it can't be visited, so pathVis = true, means once it has been vis as well, so vis != false, in any case;
-        vis[node] = true;
-        pathVis[node] = true;
+    bool dfs(int node, v<bool>& vis, v<v<int>>& adj, v<bool>& path_vis, stack<int>& stk){
+        vis[node]=true;
+        path_vis[node]=true;
         for(auto child : adj[node]){
-            dfs(child, adj, stk, vis, pathVis, cycle);
+            if(path_vis[child]){return true;}
+            if(!vis[child]){
+                bool is_cycle = dfs(child, vis, adj, path_vis, stk);
+                if(is_cycle){return true;}
+            }
         }
-        pathVis[node] = false;
         stk.push(node);
+        path_vis[node]=false;
+        return false;
     }
 
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        int n = numCourses;
-        bool vis[2001] = {false};
-        bool pathVis[2001]= {false};
-        vector<int> ans;
-        vector<int> adj[n];
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+        // also we have to consider case if the graph got cycle
+        // also we have to consider the case if the graph is disconnected;
+        // int n = pre
+        v<v<int>> adj(n);
+        v<bool> vis(n, false);
+        v<bool> path_vis(n, false);
         for(auto val : prerequisites){
-            int u = val[0];
-            int v = val[1];
-            adj[v].push_back(u);
+            int u = val[1];
+            int v = val[0];
+            adj[u].push_back(v);
         }
         stack<int> stk;
-        // if(prerequisites)
-        bool cycle = false;
-        for(int i = 0; i < n; i++){
-            if(!vis[i]){
-                dfs(i, adj, stk, vis, pathVis, cycle);
-            }
-            if(cycle){
-                return ans;
+        for(int node = 0; node < n; node++){
+            if(!vis[node]){
+                bool is_cycle = dfs(node, vis, adj, path_vis, stk);
+                if(is_cycle){return {};}
             }
         }
+        vector<int> ans;
         while(!stk.empty()){
             ans.push_back(stk.top());
             stk.pop();
