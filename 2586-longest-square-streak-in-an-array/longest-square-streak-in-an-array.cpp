4e4634d14f041f6494aaa3ea47a210185f559e1e
sanskarsingh_iitbhu21 <sanskar.singh.eee21@iitbhu.ac.in>
2024-10-28 21:21:07 +0530

    Time: 84 ms (68.89%) | Memory: 109 MB (63.34%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int longestSquareStreak(vector<int>& nums) {
         int n = nums.size();
-        set<long long> st;
+        unordered_set<int> st;
         for(auto val : nums){
             st.insert(val);
         } int maxc = 1;
         for(int i = 0; i < n; i++){
             int count = 1;
-            long long x = nums[i]*1LL;
-            while(st.find(x*x)!=st.end()){
+            int x = nums[i];
+            while(x < 1e4 && st.find(x*x)!=st.end()){
                 count++;
                 x = x*x;
             }
